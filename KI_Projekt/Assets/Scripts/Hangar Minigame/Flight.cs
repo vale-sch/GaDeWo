d5a9052b27e91cd78ae65ab7c9440500d86af19e
Marius Kögel <56830035+koegelma@users.noreply.github.com>
2021-12-13 17:47:54 +0100

    rigibody addforce in hangar minigame

--- KI_Projekt/Assets/Scripts/Hangar Minigame/Flight.cs
+++ KI_Projekt/Assets/Scripts/Hangar Minigame/Flight.cs
@@ -2,29 +2,29 @@ using UnityEngine;
 
 public class Flight : MonoBehaviour {
     [HideInInspector] public bool isFlight = false;
-    private bool hasTargetPosition = false;
-    private Vector3 targetPosition;
-    private float flySpeed = 4;
+    private Vector3 lastPosition;
+    float distanceTraveled = 0;
+    private float flySpeed = 10000;
+    private Rigidbody rb;
+    private void Start() {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
     void Update() {
         if (isFlight) Fly();
     }
 
     private void Fly() {
-        if (!hasTargetPosition) GetTargetPosition();
-
-
-        //TODO: add force or velocity to rb (change prefab to point in right z-direction)
-        if (Vector3.Distance(transform.position, targetPosition) > 0.05f) {
-            transform.position = Vector3.Lerp(transform.position, targetPosition, flySpeed * Time.deltaTime);
+        UpdateDistance();
+        if (distanceTraveled < 150) {
+            rb.AddForce(transform.forward * Time.deltaTime * flySpeed);
             return;
         }
         //Ship Taken Off Behaviour
         Destroy(gameObject);
     }
 
-    private void GetTargetPosition() {
-        Vector3 offset = new Vector3(-130, 0, 0);
-        targetPosition = transform.position + offset;
-        hasTargetPosition = true;
+    private void UpdateDistance() {
+        distanceTraveled += Vector3.Distance(transform.position, lastPosition);
+        lastPosition = transform.position;
     }
 }
